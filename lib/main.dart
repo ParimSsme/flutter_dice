248046a0af7717ec185a041f97de13a6fbf3ca7e
ParimSsme <parima.ssme@gmail.com>
2023-06-24 20:43:00 +0430

    complete design of app

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,34 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.cyan.shade800,
         appBar: AppBar(
-          title: Text('Dice'),
+          title: const Text('Dice'),
           backgroundColor: Colors.cyan.shade900,
         ),
-        body: DicePage(),
+        body: const DicePage(),
       ),
     ),
   );
 }
 
 class DicePage extends StatelessWidget {
+  
+  const DicePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset('images/dice1.png'),
+          ),),
+          Expanded(child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset('images/dice1.png'),
+          ),),
+        ],
+      ),
+    );
   }
 }

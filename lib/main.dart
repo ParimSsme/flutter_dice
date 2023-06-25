5935bc8ec93826a82a171c3e2add67f043b89997
ParimSsme <parima.ssme@gmail.com>
2023-06-25 07:23:24 +0430

    refactor codes

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,13 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
 
+  void changeDiceFace(){
+    setState((){
+      leftDiceNumber = Random().nextInt(6) + 1; // 1 - 6
+      rightDiceNumber = Random().nextInt(6) + 1; // 1 - 6
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -35,10 +42,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: (){
-                setState((){
-                  leftDiceNumber = Random().nextInt(6) + 1; // 1 - 6
-                  rightDiceNumber = Random().nextInt(6) + 1; // 1 - 6
-                });
+                changeDiceFace();
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -46,10 +50,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: (){
-                setState((){
-                  rightDiceNumber = Random().nextInt(6) + 1; // 1 - 6
-                  leftDiceNumber = Random().nextInt(6) + 1; // 1 - 6
-                });
+                changeDiceFace();
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
